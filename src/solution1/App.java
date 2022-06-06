28853fdb42add60e64a6259349c0ca08486dc5d3
Hanneli <hannelita@gmail.com>
2022-06-06 11:25:36 -0400

    Other examples of overloading constructors

--- src/solution1/App.java
+++ src/solution1/App.java
@@ -9,6 +9,14 @@ public class App {
         // System.out.println("Teaching bonus: " + f.salaryWithTeachingBonus(1.1));
         // S1Faculty f2 = new S1Faculty("Another Faculty", 50000.00);
         // System.out.println("Taxes for " + f2.getName() + " will be: " + f2.calculate_tax() + "\n");
+        System.out.println(f.getClass().getName());
+        DawsonStaff ds = new S1Faculty();
+        ds.setName("DS.");
+        ds.setSalary(40000.00);
+        System.out.println("Taxes for " + ds.getName() + " will be: " + ds.calculate_tax() + "\n");
+        // System.out.println("Teaching bonus: " + ds.salaryWithTeachingBonus(1.1));
+
+        System.out.println(ds.getClass().getName());
     }
     
 }
--- src/solution1/S1Faculty.java
+++ src/solution1/S1Faculty.java
@@ -3,17 +3,30 @@ package solution1;
 public class S1Faculty extends DawsonStaff {
     
     // They can have custom methods
-    // public double salaryWithTeachingBonus(double bonusPercentage){
-    //     return (this.getSalary() * bonusPercentage);
-    //     // this.salary * bonusPercentage; Why it won't work?
+    public double salaryWithTeachingBonus(double bonusPercentage){
+        return (this.getSalary() * bonusPercentage);
+        // this.salary * bonusPercentage; Why it won't work?
+    }
+
+    // public S1Faculty() {
+        // super("A", 1.0);
     // }
 
     public S1Faculty() {
+        super();
+        System.out.println("Faculty");
         
     }
 
+
     public S1Faculty(String name, double salary) {
         super(name, salary);
+    }
+
+    public double calculate_tax() {
+        System.out.println("Faculty method");
+        // return super.calculate_tax();
+        return this.getSalary() * 0.05;
     }
     
 }
